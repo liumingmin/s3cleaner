302a9ed8f679ea536d3bc169fa528ef43db3984c
liumingmin <liumingmin@kingsoft.com>
2022-04-07 18:12:26 +0800

    feat: init shell

---
+++ start.bat
@@ -0,0 +1,7 @@
+set S3_AK=Vg6p9p/WM55ZbiZkE8Vyzw==
+set S3_SK=r0yRc7Yxc0fB7yWRoaWJrvLlC3hShtqBFfqj13PKTLo=
+set S3_ENDPOINT=http://localhost:9005
+set S3_BUCKET=test
+set S3_REGION=zh-south-1
+
+s3cleaner.exe
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export S3_AK=Vg6p9p/WM55ZbiZkE8Vyzw==
+export S3_SK=r0yRc7Yxc0fB7yWRoaWJrvLlC3hShtqBFfqj13PKTLo=
+export S3_ENDPOINT=http://localhost:9005
+export S3_BUCKET=test
+export S3_REGION=zh-south-1
+
+./s3cleaner
