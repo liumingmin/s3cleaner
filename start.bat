f9b6118c99d871f40e7de050afd76fad70f659c7
liumingmin <liumingmin@kingsoft.com>
2022-04-24 10:00:25 +0800

    feat: config expday

--- start.bat
+++ start.bat
@@ -5,13 +5,13 @@ set S3_BUCKET=test
 set S3_REGION=zh-south-1
 set S3_COPY_BUCKET=testtmp
 
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=0  > output0.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=1  > output1.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=2  > output2.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=3  > output3.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=4  > output4.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=5  > output5.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=6  > output6.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=7  > output7.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=8  > output8.txt
-./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=9  > output9.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=0  > output0.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=1  > output1.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=2  > output2.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=3  > output3.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=4  > output4.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=5  > output5.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=6  > output6.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=7  > output7.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=8  > output8.txt
+./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=9  > output9.txt
--- start.sh
+++ start.sh
@@ -7,13 +7,13 @@ export S3_BUCKET=test
 export S3_REGION=zh-south-1
 export S3_COPY_BUCKET=testtmp
 
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=0  > output0.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=1  > output1.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=2  > output2.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=3  > output3.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=4  > output4.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=5  > output5.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=6  > output6.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=7  > output7.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=8  > output8.txt &
-nohup ./s3cleaner --mode=2 --page=100000 --hashn=10 --hashi=9  > output9.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=0  > output0.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=1  > output1.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=2  > output2.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=3  > output3.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=4  > output4.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=5  > output5.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=6  > output6.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=7  > output7.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=8  > output8.txt &
+nohup ./s3cleaner --mode=2 --page=100000 --expday=1125 --hashn=10 --hashi=9  > output9.txt &
