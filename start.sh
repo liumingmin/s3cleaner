547c7c2761cb00f2f4012cda6e22de9e231cf9f8
liumingmin <liumingmin@kingsoft.com>
2022-04-24 09:57:50 +0800

    feat: config

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export S3_AK=Vg6p9p/WM55ZbiZkE8Vyzw==
-export S3_SK=r0yRc7Yxc0fB7yWRoaWJrvLlC3hShtqBFfqj13PKTLo=
+export S3_AK=
+export S3_SK=
 export S3_ENDPOINT=http://localhost:9005
 export S3_BUCKET=test
 export S3_REGION=zh-south-1
